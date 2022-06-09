7ce6294de82e6fb7342d11ba7bbc8f5499d1e468
Noah Walji <noahwalji@gmail.com>
2022-06-08 21:47:32 -0400

    Image URL Fixes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,7 +7,7 @@ function Portfolio(props) {
             <div className="row">
                 <div className="col-lg-6 align-items-stretch">
                     <div className="card">
-                        <img className="card-img-top" src="./amiibotrackerIcon.png" alt="Amiibo Tracker"/>
+                        <img className="card-img-top" src={process.env.PUBLIC_URL +"./amiiboTrackerIcon.png"} alt="Amiibo Tracker"/>
                         <div className="card-body">
                             <h5 className="card-title">Amiibo Tracker</h5>
                             <p className="card-text">Web application that allows users to explore, save and display their amiibo collections. Users can sign-up, login and save figures to their wishlist and collection!</p>
@@ -19,7 +19,7 @@ function Portfolio(props) {
                 </div>
                 <div className="col-lg-6 d-flex align-items-stretch">
                     <div className="card">
-                        <img className="card-img-top" src="./personalSiteIcon.png" alt="Amiibo Tracker"/>
+                        <img className="card-img-top" src={process.env.PUBLIC_URL +"./personalSiteIcon.png"} alt="Amiibo Tracker"/>
                         <div className="card-body">
                             <h5 className="card-title">Personal Website</h5>
                             <p className="card-text">A single paged web application that you're viewing right now! The site outlines my portfolio and accomplishments</p>
@@ -34,20 +34,20 @@ function Portfolio(props) {
 
             <div className="technologies">
                 <h3>Frontend Technologies</h3>
-                <img className="frontEndimg" src="./html.png" alt="HTML"></img>
-                <img className="frontEndimg" src="./css.png" alt="CSS"></img>
-                <img className="frontEndimg" src="./javascript.png" alt="Javascript"></img>
-                <img className="frontEndimg" src="./reactjs.png" alt="ReactJS"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./html.png"} alt="HTML"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./css.png"} alt="CSS"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./javascript.png"} alt="Javascript"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./reactjs.png"} alt="ReactJS"></img>
             </div>
             <div className="technologies">
                 <h3>Backend Technologies</h3>
-                <img className="frontEndimg" src="./php.png" alt="PHP"></img>
-                <img className="frontEndimg" src="./nodejs.png" alt="NodeJS"></img>
-                <img className="frontEndimg" src="./csharp.png" alt="C#"></img>
-                <img className="frontEndimg" src="./c.png" alt="C"></img>
-                <img className="frontEndimg" src="./java.png" alt="Java"></img>
-                <img className="frontEndimg" src="./python.png" alt="Python"></img>
-                <img className="frontEndimg" src="./mysql.png" alt="MySQL"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./php.png"} alt="PHP"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./nodejs.png"} alt="NodeJS"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./csharp.png"} alt="C#"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./c.png"} alt="C"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./java.png"} alt="Java"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./python.png"} alt="Python"></img>
+                <img className="frontEndimg" src={process.env.PUBLIC_URL +"./mysql.png"} alt="MySQL"></img>
             </div>
         </div>
     )
